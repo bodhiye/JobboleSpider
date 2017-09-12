11a98439dd69234fc8d9ea1f1e0cec8049a39dfd
叶琼州 <bodhiye@qq.com>
2017-09-12 16:22:36 +0800

    JobboleSpider
    爬虫小练习

---
+++ Spider/items.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+import datetime
+import re
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+from scrapy.loader import ItemLoader
+
+class SpiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+def date_convert(value):
+    try:
+        create_date = datetime.datetime.strptime(value, "%Y/%m/%d").date()
+    except Exception as e:
+        create_date = datetime.datetime.now().date()
+    return create_date
+
+def get_nums(value):
+    match_re = re.match(".*?(\d+).*", value)
+    if match_re:
+        nums = int(match_re.group(1))
+    else:
+        nums = 0
+    return nums
+
+def get_url(value):
+    if value.find("http") == -1:
+        value = "http:" + value
+    return value
+
+def remove_comment_tags(value):
+    if "评论" in value:
+        return ""
+    else:
+        return value
+
+def return_value(value):
+    return value
+
+class ArticleItemLoader(ItemLoader):
+    #自定义itemLoader
+    default_output_processor = TakeFirst()
+
+class JobBoleArticleItem(scrapy.Item):
+    title = scrapy.Field()
+    create_date = scrapy.Field(
+        input_processor=MapCompose(date_convert)
+    )
+    url = scrapy.Field()
+    url_object_id = scrapy.Field()
+    front_image_url = scrapy.Field(
+        input_processor=MapCompose(get_url),
+        output_processor=MapCompose(return_value)
+    )
+    front_image_path = scrapy.Field()
+    praise_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    comment_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    fav_nums = scrapy.Field(
+        input_processor=MapCompose(get_nums)
+    )
+    tags = scrapy.Field(
+        input_processor=MapCompose(remove_comment_tags),
+        output_processor=Join(",")
+    )
+    content = scrapy.Field()
---
+++ Spider/pipelines.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import codecs
+import json
+import MySQLdb
+import MySQLdb.cursors
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exporters import JsonItemExporter
+from twisted.enterprise import adbapi
+
+class SpiderPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+class JsonWithEncodingPipeline(object):
+    #自定义json文件的导出
+    def __init__(self):
+        self.file = codecs.open('article.json', 'w', encoding="utf-8")
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(lines)
+        return item
+    def spider_closed(self, spider):
+        self.file.close()
+
+class MysqlPipeline(object):
+    #采用同步的机制写入mysql
+    def __init__(self):
+        self.conn = MySQLdb.connect('localhost', 'root', '795200', 'spider', charset='utf8', use_unicode=True)
+        self.cursor = self.conn.cursor()
+    def process_item(self, item, spider):
+        insert_sql = """
+            insert into article(title, create_date, url, url_object_id, front_image_url, front_image_path, praise_nums, comment_nums, fav_nums, tags, content)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        self.cursor.execute(insert_sql, (item["title"], item["create_date"], item["url"], item["url_object_id"], item["front_image_url"], item["front_image_path"], item["praise_nums"], item["comment_nums"], item["fav_nums"], item["tags"], item["content"]))
+        self.conn.commit()
+
+class MysqlTwistedPipeline(object):
+    def __init__(self, dbpool):
+        self.dbpool = dbpool
+
+    @classmethod
+    def from_settings(cls, settings):
+        dbparms = dict(
+            host = settings["MYSQL_HOST"],
+            db = settings["MYSQL_DBNAME"],
+            user = settings["MYSQL_USER"],
+            passwd = settings["MYSQL_PASSWORD"],
+            charset = 'utf8mb4',
+            cursorclass = MySQLdb.cursors.DictCursor,
+            use_unicode = True,
+        )
+        dbpool = adbapi.ConnectionPool("MySQLdb", **dbparms)
+        return cls(dbpool)
+
+    def process_item(self, item, spider):
+        #使用twisted将mysql插入变成异步执行
+        query = self.dbpool.runInteraction(self.do_insert, item)
+        query.addErrback(self.handle_error)#处理异常
+
+    def handle_error(self, failure):
+        #处理异步插入的异常
+        print(failure)
+
+    def do_insert(self, cursor, item):
+        #执行具体的插入
+        insert_sql = """
+                    insert into article(title, create_date, url, url_object_id, front_image_url, front_image_path, praise_nums, comment_nums, fav_nums, tags, content)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """
+        cursor.execute(insert_sql, (
+        item["title"], item["create_date"], item["url"], item["url_object_id"], item["front_image_url"],
+        item["front_image_path"], item["praise_nums"], item["comment_nums"], item["fav_nums"], item["tags"],
+        item["content"]))
+
+class JsonExporterPipeline(object):
+    #调用scrapy提供的json export导出json文件
+    def __init__(self):
+        self.file = open('articleexport.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding="utf-8", ensure_ascii=False)
+        self.exporter.start_exporting()
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+class ArticleImagePipeline(ImagesPipeline):
+    def item_completed(self, results, item, info):
+        if "front_image_path" in item:
+            for ok, value in results:
+                if ok:
+                    image_file_path = value["path"]
+                    item["front_image_path"] = image_file_path
+                else:
+                    item["front_image_path"] = ""
+        return item
